35d73ed7da5909fec1b2a40aaa476714d86bbed3
Felipe <felipebayres@gmail.com>
2019-05-29 18:35:38 -0300

    Funcao de deletar usuario foi consertada

--- src/main.cpp
+++ src/main.cpp
@@ -4,7 +4,16 @@
 
 int main(void) {
 	usuario user;
-	user = usuario("Felipe","Felipe1");
-		user.EditarSenha("Vinica12345");	
+	user = usuario("01234567894","Felipe4");
+	user.CriaUsuario();
+	user = usuario("01234567891","Felipe5");
+	user.CriaUsuario();
+	user = usuario("01234567892","Felipe5");
+	user.CriaUsuario();
+	user = usuario("01234567891","Felipe5");
+	user.DeletaUsuario();
+	user = usuario("TESTETESTE1","Felipe5");
+	user.CriaUsuario();
+	user.DeletaUsuario();
 	return 0;
 }
