7a560c4003c678e30400798e655f03d35e3a88bc
helioadson <helioadson@hormail.com>
2019-06-01 21:50:39 +0000

    Menu implementado com funções até então criadas para usuario e eventos.

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,12 @@
 #include "funcoes.h"
 #include "evento.hpp"
 #include "usuario.hpp"
+#include "menu.hpp"
 
 int main(void) {
 	usuario user;
 	user = usuario("Felipe","Felipe1");
 		user.EditarSenha("Vinica12345");	
+	mainmenu();
 	return 0;
 }
